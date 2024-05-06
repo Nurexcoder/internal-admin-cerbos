3687ba2c9bd320c2adaf9a67f99257cec07f8176
PrathamSikka24 <prathamsikka2004@gmail.com>
2024-05-06 19:19:38 +0530

    Styling

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,16 @@
-import logo from "../assets/image.png";
-
+import logo from "../assets/cerbos-logo.png";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="logo-container">
         <img src={logo} className="App-logo" alt="logo" />
-        <span style={{fontWeight: 'bold', fontSize: '1.3rem'}}>Dev<span style={{fontWeight: 'lighter'}}>Zero</span></span>
+        <span style={{ fontWeight: "bold", fontSize: "1.3rem" }}>
+          Dev<span style={{ fontWeight: "lighter" }}>Zero</span>
+        </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
